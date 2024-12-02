faa40596e5589c8ddef51228dc6734574ec90814
yuriy-kulakovskyi <yurakulak42@gmail.com>
2024-12-02 16:07:59 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useWebSocket } from "./hooks/useWebSocket";
+import Button from "./components/Button";
+import TransactionsList from "./components/TransactionsList";
 
-function App() {
+const App: React.FC = () => {
+  const { transactions, totalSum, isSubscribed, start, stop, reset } = useWebSocket();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-[20px]">
+      <h1 className="mb-[20px]">Bitcoin Transactions</h1>
+      <div>
+        <Button onClick={start} disabled={isSubscribed}>
+          Запуск
+        </Button>
+        <Button onClick={stop} disabled={!isSubscribed}>
+          Зупинка
+        </Button>
+        <Button onClick={reset}>Скинути</Button>
+      </div>
+
+      <h2 className="mb-[20px]">Сума транзакцій: {totalSum.toFixed(8)} BTC</h2>
+
+      <TransactionsList transactions={transactions} />
     </div>
   );
-}
+};
 
 export default App;
